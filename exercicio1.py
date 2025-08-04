0faa284d986fdb03c059b80b77d80e4a3ad3767b
tarso em vocacao <rodriguesdosanjostarso@gmail.com>
2025-08-04 16:54:13 -0300

    subir atividade

---
+++ exercicio1.py
@@ -0,0 +1,10 @@
+# Programa simples para exibir nome e tipo de dado
+
+# Entrada do nome do usuário
+nome = input("Digite seu nome: ")
+
+# Exibindo mensagem personalizada
+print("Olá,", nome + "! Seja muito bem-vindo ao mundo da programação em Python!")
+
+# Exibindo o tipo da variável 'nome'
+print("Tipo de dado da variável 'nome':", type(nome))
